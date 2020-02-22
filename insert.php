01a9892c03e23105888a8fd2ed4bb20367e9dd7c
Saud Iqbal <saudiqbal@users.noreply.github.com>
2020-02-22 16:34:57 -0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,38 +7,16 @@ $link = $_POST['link'];
 if(empty($title))
 {
 header("Location: error.php?error=notitle");
-exit();
+exit;
 }
 if(empty($link))
 {
 header("Location: error.php?error=nolink");
 exit;
 }
-  
-$referer = $_SERVER['HTTP_REFERER'];
-
-//Check if browser sends referrer url or not
-if ($referer == "") //If not, set referrer as your domain
-    $domain = $yoursite;
-else
-    $domain = parse_url($referer); //If yes, parse referrer
-
-if($domain['host'] == $yoursite || $domain['host'] == $yoursite2)
-{
-    //open the database
-    $db = new PDO("sqlite:$dbname");
+//open the database
+$db = new PDO("sqlite:$dbname");
 $link = preg_replace('/^(?!https?:\/\/)/', 'http://', $link);
-	$db->exec("INSERT INTO BookmarksTable (Title, Link) VALUES ('$title' ,'$link')");
-	
-    // close the database connection
-    $db = NULL;
-
+$db->exec("INSERT INTO BookmarksTable (Title, Link) VALUES ('$title' ,'$link')");
 header("Location: $mainpage"); 
-}
-else
-{
-    //The referrer is not your site, we redirect to your home page
-    header("Location: $yoursite2");
-    exit(); //Stop running the script
-}
 ?>
